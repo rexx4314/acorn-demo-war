6ba77e5eff4d9620075b422037b28a2efbe11768
rex <rexx4314@gmail.com>
2024-10-15 18:19:06 +0900

    add profile config

--- src/main/java/com/acorn/demo/war/controller/IndexController.java
+++ src/main/java/com/acorn/demo/war/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.acorn.demo.war.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,10 +8,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class IndexController {
 
+    @Value("${acorn.message}")
+    private String acornMessage;
+
     @RequestMapping("/")
     @ResponseBody
     public String index() {
-        return "Welcome Acorn!";
+        return "Welcome Acorn! Profile: " + acornMessage;
     }
 
     @RequestMapping("/hello")
